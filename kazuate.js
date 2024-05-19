a3112e3dee704dd56581e223708ccfdd5e26f282
G385012023 <238501@st.takushoku-u.ac.jp>
2024-05-19 12:57:12 +0900

    k

--- kazuate.js
+++ kazuate.js
@@ -17,6 +17,16 @@ hantei();
 function hantei() {
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
   let yoso = 4;
+  kaisu1
+  if(yoso===kotae){
+    console.log("正解です。おめでとう！");
+  }
+  else if(yoso>kotae){
+    console.log("まちがい。答えはもっと小さいですよ");
+  }
+  else if (yoso<kotae){
+    console.log("まちがい。答えはもっと大きいですよ");
+  }
   
   // 課題3-1: 正解判定する
   // kotae と yoso が一致するかどうか調べて結果を出力

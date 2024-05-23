82b8f762e27047e806da7948a22f743daa196a1e
G385012023 <238501@st.takushoku-u.ac.jp>
2024-05-23 16:13:48 +0900

    k

--- kazuate.js
+++ kazuate.js
@@ -20,10 +20,11 @@ function hantei() {
   kaisu = kaisu+1;
 
   console.log(kaisu+'回数の予想'+yoso);
-  
+
   // 課題3-1: 正解判定する
   // kotae と yoso が一致するかどうか調べて結果を出力
   // 課題3-1における出力先はコンソール
+  let seikaisu =0;
   if(kazu()){
     return console.log(sego(yoso))
   }

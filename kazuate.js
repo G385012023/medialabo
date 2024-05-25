03721ea960bffe975a59bb5ccd16b198c9b9d3af
G385012023 <238501@st.takushoku-u.ac.jp>
2024-05-25 14:43:01 +0900

    k

--- kazuate.js
+++ kazuate.js
@@ -1,47 +1,38 @@
+l// 答え
 let kotae = Math.floor(Math.random()*10) + 1;
 console.log('答え（デバッグ用）: ' + kotae);
 
 // 入力回数（予想回数）
 let kaisu = 0;
-let seikaisu = 0;
 
 // 予想を4回実行する
 // 将来以下の hantei(); の4回の呼び出しを全て削除する
 // 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
-console.log(hantei());
-console.log(hantei());
-console.log(hantei());
-console.log(hantei());
+hantei();
+hantei();
+hantei();
+hantei();
 
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
   let yoso = 4;
-  kaisu++;
-
-  console.log(kaisu + '回数の予想' + yoso);
-
+  
   // 課題3-1: 正解判定する
+  if(yoso === kotae){
+    console.log('正解です．おめでとう!');
+    kaisu = kaisu + 1;
+  }
   // kotae と yoso が一致するかどうか調べて結果を出力
+  if(kotae ===!yoso){
+    if(kotae>yoso){
+    kaisu = kaisu + 1;
+    console.log('まちがい．答えはもっと小さいですよ');
+    }
+    else{
+    kaisu = kaisu + 1;
+    console.log('まちがい．答えはもっと大きいですよ');
+    }
+  }
   // 課題3-1における出力先はコンソール
-  if (kazu()) {
-    return sego(yoso);
-  }
-  return '答えは' + kotae + 'でした。すでにゲームは終わっています。';
-}
-
-function kazu() {
-  return kaisu <= 3 && seikaisu === 0;
-}
-
-function sego(x) {
-  if (x === kotae) {
-    seikaisu++;
-    return '正解';
-  }
-  return '間違い。答えはもっと' + saizu(x) + 'ですよ';
-}
-
-function saizu(x) {
-  return x <= kotae ? '大きい' : '小さい';
 }

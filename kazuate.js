264bb46e71d457adb82366f5e88cd44cfd183bef
G385012023 <238501@st.takushoku-u.ac.jp>
2024-05-30 13:50:40 +0900

    K

--- kazuate.js
+++ kazuate.js
@@ -22,32 +22,27 @@ function hantei() {
   // 課題3-1: 正解判定する
   // kotae と yoso が一致するかどうか調べて結果を出力
    // 課題3-1における出力先はコンソール
+   kaisu = kaisu + 1;
     if(kotae === yoso && kaisu<=3){
-    kaisu = kaisu + 1;
     console.log(kaisu+'回目の予想：'+ yoso)
     console.log( '正解です．おめでとう!');
     }
 
     else if ( kotae>yoso && kaisu<3){
-      kaisu = kaisu + 1;
       console.log(kaisu+'回目の予想：'+ yoso)
       console.log( 'まちがい．答えはもっと大きいですよ');
     }
 
     else if (kotae !==yoso && kotae<yoso && kaisu<3){
-      kaisu = kaisu + 1;
       console.log(kaisu+'回目の予想：'+ yoso)
       console.log('まちがい．答えはもっと小さいですよ');
     }
-
     else if(kaisu===3 && kotae !==yoso){
-      kaisu = kaisu + 1;
       console.log(kaisu+'回目の予想：'+ yoso)
       console.log ('まちがい．残念でした答えは'+ kotae +'です．');
     }
 
     else if(kaisu >= 4){
-      kaisu = kaisu + 1;
       console.log(kaisu+'回目の予想：'+ yoso)
       console.log('答えは'+kotae+'でした．すでにゲームは終わっています');
     }

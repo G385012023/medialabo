c6b65eb26ad30f324a162c5a24691ee6d9ec0197
G385012023 <238501@st.takushoku-u.ac.jp>
2024-06-12 20:03:28 +0900

    更新

--- kazuate.js
+++ kazuate.js
@@ -14,14 +14,12 @@ function hantei() {
   // kotae と yoso が一致するかどうか調べて結果を出力
   kaisu += 1;
 
-  let resultElement = document.querySelector('#result');
+  let kaisuElement = document.querySelector('#kaisu');
+  let answerElement = document.querySelector('#answer');
   let feedbackElement = document.querySelector('#feedback');
 
-  if (kaisu <= 3) {
-    resultElement.textContent = kaisu + '回目の予想: ' + yoso;
-  } else {
-    resultElement.textContent = '';
-  }
+  kaisuElement.textContent = kaisu + '回目の予想:';
+  answerElement.textContent = yoso;
 
   if (kotae === yoso && kaisu <= 3) {
     feedbackElement.textContent = '正解です．おめでとう!';
@@ -35,8 +33,11 @@ function hantei() {
     feedbackElement.textContent = '答えは' + kotae + 'でした．すでにゲームは終わっています';
   }
 
-  // コンソールにデバッグ用の答えを表示する
+
   console.log('答え（デバッグ用）: ' + kotae);
 }
 
 document.querySelector('#submit').addEventListener('click', hantei);
+
+
+

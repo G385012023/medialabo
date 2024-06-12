31269c672b8214a7b2077fa697f8414f607b4d38
G385012023 <238501@st.takushoku-u.ac.jp>
2024-06-12 20:00:19 +0900

    更新

--- kazuate.js
+++ kazuate.js
@@ -27,9 +27,6 @@ function hantei() {
   } else if (kaisu >= 4) {
     feedbackElement.textContent = '答えは' + kotae + 'でした．すでにゲームは終わっています';
   }
-
-  // コンソールにデバッグ用の答えを表示する
-  console.log('答え（デバッグ用）: ' + kotae);
 }
 
 document.querySelector('#submit').addEventListener('click', hantei);

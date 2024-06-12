f80f91308bc3c0db41db5d00480a40ccb8f58a86
G385012023 <238501@st.takushoku-u.ac.jp>
2024-06-12 21:23:45 +0900

    k

--- kazuate.js
+++ kazuate.js
@@ -4,6 +4,9 @@ let kotae = Math.floor(Math.random() * 10) + 1;
 // 入力回数（予想回数）
 let kaisu = 0;
 
+// 初回の0回目の予想をコンソールに表示
+console.log(kaisu + '回目の予想:');
+
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
   // テキストボックスを取得してその値を yoso に代入する
@@ -16,8 +19,9 @@ function hantei() {
 
   let feedbackElement = document.querySelector('#feedback');
 
-  // コンソールに予想回数と入力値を表示
-  console.log(kaisu + '回目の予想: ' + yoso);
+  if (kaisu <= 3) {
+    console.log(kaisu + '回目の予想: ' + yoso);
+  }
 
   if (kotae === yoso && kaisu <= 3) {
     feedbackElement.textContent = '正解です．おめでとう!';
@@ -29,11 +33,11 @@ function hantei() {
     feedbackElement.textContent = 'まちがい．答えはもっと小さいですよ';
     console.log('まちがい．答えはもっと小さいですよ');
   } else if (kaisu === 3 && kotae !== yoso) {
-    feedbackElement.textContent = '残念でした。答えは' + kotae + 'です。';
+    feedbackElement.textContent = 'まちがい．残念でした答えは' + kotae + 'です．';
     console.log('残念でした。答えは' + kotae + 'です。');
   } else if (kaisu >= 4) {
-    feedbackElement.textContent = '答えは' + kotae + 'でした。すでにゲームは終わっています';
-    console.log(kaisu + '回目の予想: 答えは' + kotae + 'でした。すでにゲームは終わっています。');
+    feedbackElement.textContent = '答えは' + kotae + 'でした．すでにゲームは終わっています';
+    console.log(kaisu + '回目の予想: 答えは' + kotae + 'でした．すでにゲームは終わっています。');
   }
 
   // コンソールにデバッグ用の答えを表示する

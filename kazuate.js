5d2a05b8063396c1574dc681be6e0b566e0941a5
G385012023 <238501@st.takushoku-u.ac.jp>
2024-06-12 19:08:01 +0900

    更新

--- kazuate.js
+++ kazuate.js
@@ -1,54 +1,38 @@
 // 答え
-let kotae = Math.floor(Math.random()*10) + 1;
+let kotae = Math.floor(Math.random() * 10) + 1;
 console.log('答え（デバッグ用）: ' + kotae);
 
 // 入力回数（予想回数）
 let kaisu = 0;
 
-// 予想を4回実行する
-// 将来以下の hantei(); の4回の呼び出しを全て削除する
-// 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
-hantei();
-hantei();
-hantei();
-hantei(); 
-
-
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
-  // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
-  let yoso = 4;
-  
+  // テキストボックスに指定された数値を yoso に代入する
+  let yoso = Number(document.getElementById('yoso').value);
+
   // 課題3-1: 正解判定する
   // kotae と yoso が一致するかどうか調べて結果を出力
-   // 課題3-1における出力先はコンソール
-   kaisu = kaisu + 1;
-    if(kotae === yoso && kaisu<=3){
-    console.log(kaisu+'回目の予想：'+ yoso)
-    console.log( '正解です．おめでとう!');
-    }
+  kaisu = kaisu + 1;
 
-    else if ( kotae>yoso && kaisu<3){
-      console.log(kaisu+'回目の予想：'+ yoso)
-      console.log( 'まちがい．答えはもっと大きいですよ');
-    }
+  let kaisuElement = document.getElementById('kaisu');
+  let answerElement = document.getElementById('answer');
+  let resultElement = document.getElementById('result');
 
-    else if (kotae !==yoso && kotae<yoso && kaisu<3){
-      console.log(kaisu+'回目の予想：'+ yoso)
-      console.log('まちがい．答えはもっと小さいですよ');
-    }
-    else if(kaisu===3 && kotae !==yoso){
-      console.log(kaisu+'回目の予想：'+ yoso)
-      console.log ('まちがい．残念でした答えは'+ kotae +'です．');
-    }
+  kaisuElement.textContent = kaisu + '回目の予想:';
+  answerElement.textContent = yoso;
 
-    else if(kaisu >= 4){
-      console.log(kaisu+'回目の予想：'+ yoso)
-      console.log('答えは'+kotae+'でした．すでにゲームは終わっています');
-    }
+  if (kotae === yoso && kaisu <= 3) {
+    resultElement.textContent = '正解です．おめでとう!';
+  } else if (kotae > yoso && kaisu < 3) {
+    resultElement.textContent = 'まちがい．答えはもっと大きいですよ';
+  } else if (kotae < yoso && kaisu < 3) {
+    resultElement.textContent = 'まちがい．答えはもっと小さいですよ';
+  } else if (kaisu === 3 && kotae !== yoso) {
+    resultElement.textContent = 'まちがい．残念でした答えは' + kotae + 'です．';
+  } else if (kaisu >= 4) {
+    resultElement.textContent = '答えは' + kotae + 'でした．すでにゲームは終わっています';
+  }
 }
 
-let k = document.querySelectorAll('span#kaisu');
-let a = document.querySelectorAll('span#answer'); 
-let p1 = document.createElement('p');
-let p2 = document.createElement('p'); 
+document.getElementById('submit').addEventListener('click', hantei);
+

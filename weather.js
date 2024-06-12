988f100495dbcec9c1e8ddab47f610ff5afa6647
G385012023 <238501@st.takushoku-u.ac.jp>
2024-06-12 22:30:30 +0900

    k

--- weather.js
+++ weather.js
@@ -1,20 +1,21 @@
 const cityData = {
-  "Egypt": { "name": "Cairo", "temp": 30, "weather": "晴れ", "humidity": 50, "wind": 5 },
-  "Russia": { "name": "Moscow", "temp": -5, "weather": "雪", "humidity": 80, "wind": 10 },
-  "South Africa": { "name": "Johannesburg", "temp": 25, "weather": "曇り", "humidity": 60, "wind": 7 },
-  "China": { "name": "Beijing", "temp": 20, "weather": "霧", "humidity": 70, "wind": 3 },
-  "Japan": { "name": "Tokyo", "temp": 15, "weather": "晴れ", "humidity": 40, "wind": 2 },
-  "Singapore": { "name": "Singapore", "temp": 28, "weather": "雨", "humidity": 90, "wind": 4 },
-  "Australia": { "name": "Sydney", "temp": 22, "weather": "曇り", "humidity": 65, "wind": 6 },
-  "United Kingdom": { "name": "London", "temp": 10, "weather": "雨", "humidity": 85, "wind": 8 },
-  "France": { "name": "Paris", "temp": 18, "weather": "晴れ", "humidity": 55, "wind": 5 },
-  "Brazil": { "name": "Brasília", "temp": 35, "weather": "晴れ", "humidity": 45, "wind": 3 },
-  "United States": { "name": "Washington D.C.", "temp": 12, "weather": "曇り", "humidity": 60, "wind": 7 }
+  "360630": { "name": "Cairo", "temp": 30, "weather": "晴れ", "humidity": 50, "wind": 5 },
+  "524901": { "name": "Moscow", "temp": -5, "weather": "雪", "humidity": 80, "wind": 10 },
+  "993800": { "name": "Johannesburg", "temp": 25, "weather": "曇り", "humidity": 60, "wind": 7 },
+  "1816670": { "name": "Beijing", "temp": 20, "weather": "霧", "humidity": 70, "wind": 3 },
+  "1850147": { "name": "Tokyo", "temp": 15, "weather": "晴れ", "humidity": 40, "wind": 2 },
+  "1880252": { "name": "Singapore", "temp": 28, "weather": "雨", "humidity": 90, "wind": 4 },
+  "2147714": { "name": "Sydney", "temp": 22, "weather": "曇り", "humidity": 65, "wind": 6 },
+  "2643743": { "name": "London", "temp": 10, "weather": "雨", "humidity": 85, "wind": 8 },
+  "2968815": { "name": "Paris", "temp": 18, "weather": "晴れ", "humidity": 55, "wind": 5 },
+  "3451190": { "name": "Rio de Janeiro", "temp": 35, "weather": "晴れ", "humidity": 45, "wind": 3 },
+  "5128581": { "name": "New York", "temp": 12, "weather": "曇り", "humidity": 60, "wind": 7 },
+  "5368361": { "name": "Los Angeles", "temp": 25, "weather": "晴れ", "humidity": 30, "wind": 5 }
 };
 
 document.querySelector('#search-button').addEventListener('click', function() {
-  let country = document.querySelector('#country-select').value;
-  let cityInfo = cityData[country];
+  let cityId = document.querySelector('#city-select').value;
+  let cityInfo = cityData[cityId];
   displayWeatherData(cityInfo);
 });
 
